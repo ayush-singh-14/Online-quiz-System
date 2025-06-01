c6267c12f82a3c26233838f395201d15ec669934
Akash Singh <akashrautana01@gmail.com>
2025-06-01 22:35:31 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,60 @@
+let questions = [
+    {
+        numb:1,
+        question:"What does html stand for?",
+        answer:"C.Hyper Text Markup Language",
+        options:[
+            "A.Hyper Type Multi Language",
+            "B.Hyper Type Multiple Language",
+            "C.Hyper Text Markup Language",
+            "D.Home Text Multi Language",
+        ]
+    },
+    {
+        numb:2,
+        question:"What does CSS Stand for?",
+        answer:"A.Casecoading Style Sheet",
+        options:[
+            "A.Casecoading Style Sheet",
+            "B.Cute style Sheet",
+            "C.Computer Style Sheet",
+            "D.Codehal Style Sheet",
+        ]
+    },
+    {
+        numb:3,
+        question:"What does PHP stand for?",
+        answer:"A.Hypertext preprocessor",
+        options:[
+            "A.Hypertext preprocessor",
+            "B.Hometext Programming",
+            "C.Hypertext Preprogramming",
+            "D.Programming Hypertext Preprocessor",
+            
+        ]
+    },
+    {
+        numb:4,
+        question:"What does SQL stand for?",
+        answer:"D.Structured Query Language",
+        options:[
+            "A.Strength Querey language",
+            "B.Stylesheet Query Language",
+            "C.Sciene Questions Language",
+            "D.Structured Query Language",
+            
+        ]
+    },
+    {
+        numb:5,
+        question:"What does XML stand for?",
+        answer:"D.Extensible Markup Language",
+        options:[
+            "A.Excellent Markup Language",
+            "B.Explore Markup Language",
+            "C.Extra Markup Language",
+            "D.Extensible Markup Language",
+            
+        ]
+    }
+];
